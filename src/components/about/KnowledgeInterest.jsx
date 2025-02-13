36005b4ac11e5b78977649a2f9cddfe24324f71f
ericrudawski <erudawski@gmail.com>
2025-02-12 18:32:20 -0800

    intital commit

---
+++ src/components/about/KnowledgeInterest.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+const KnowledgeInterest = () => {
+  const knowledgeContent = [
+    {
+      id: 1,
+      colClass: "left",
+      title: "Learning",
+      content: [
+        " React / JS",
+        " Meditation",
+        " How to keep a plant alive",
+        " CAD modeling / Shapr3D",
+      ],
+    },
+    {
+      id: 2,
+      colClass: "right",
+      title: "Building",
+      content: [
+        " Floating living room shelves",
+        " Custom fit record player cabinet",
+        " A tolerance for cardio",
+        " More features for this site",
+      ],
+    },
+  ];
+
+  return (
+    <>
+      {knowledgeContent.map((item) => (
+        <div className={item.colClass} key={item.id}>
+          <div className="tokyo_section_title">
+            <h3>{item.title}</h3>
+          </div>
+          <div className="tokyo_tm_skill_list">
+            <ul>
+              {item.content.map((val, i) => (
+                <li key={i}>
+                  <span>
+                    <img
+                      className="svg"
+                      src="assets/img/svg/rightarrow.svg"
+                      alt="arrow"
+                    />
+                    {val}
+                  </span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default KnowledgeInterest;
---
+++ src/components/about/PersonalInfo.jsx
@@ -0,0 +1,97 @@
+import React from "react";
+
+const PersonalInfo = () => {
+  const personalInfoContent = [
+    {
+      id: 1,
+      colClass: "left",
+      info: [
+        {
+          id: 2,
+          name: "Languages",
+          content: "English, Polish, Spanish, French",
+        },
+        // {
+        //   // id: 2,
+        //   // name: "Height",
+        //   // content: "6'7\"",
+        // },
+        {
+          id: 1,
+          name: "Located In",
+          content: "San Jose",
+        },
+        {
+          id: 4,
+          name: "Email",
+          content: (
+            <>
+              <a href="mailto:erudawski@gmail.com">erudawski@gmail.com</a>
+            </>
+          ),
+        },
+        {
+          id: 5,
+          name: "Phone",
+          content: (
+            <>
+              {/* <a href="tel:+1 408 693 9389">408-693-9389</a> */}
+            Available upon request
+            </>
+          ),
+        },
+      ],
+    },
+    {
+      id: 2,
+      colClass: "right",
+      info: [
+        // {
+        //   id: 1,
+        //   name: "Nationality",
+        //   content: "Poland",
+        // },
+        {
+          id: 2,
+          name: "College",
+          content: "San Jose State University",
+        },
+        {
+          id: 3,
+          name: "Degree",
+          content: "BS, Finance",
+        },
+        {
+          id: 4,
+          name: "Interests",
+          content: "🪚 🎧 ⚾️",
+        },
+        {
+          id: 5,
+          name: "Now reading",
+          content: "Wherever You Go, There You Are",
+        },
+      ],
+    },
+  ];
+  return (
+    <>
+      {personalInfoContent.map((item) => (
+        <div className={item.colClass} key={item.id}>
+          <div className="tokyo_tm_info">
+            <ul>
+              {item?.info?.map((value) => (
+                <li key={value.id}>
+                  <span>{value.name}:</span>
+                  <span>{value.content}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default PersonalInfo;
